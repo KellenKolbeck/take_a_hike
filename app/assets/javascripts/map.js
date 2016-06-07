14f0c2bcca500475dd0851db410a5e9dfd2223c7
KellenKolbeck <kellengkolbeck@gmail.com>
2016-06-07 16:02:22 -0700

    Add javascript to center map on user's location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,24 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
-  if (document.querySelectorAll('#map').length > 0)
-  {
-    if (document.querySelector('html').lang)
-      lang = document.querySelector('html').lang;
-    else
-      lang = 'en';
+    if (document.querySelectorAll('#map').length > 0) {
+        if (document.querySelector('html').lang)
+            lang = document.querySelector('html').lang;
+        else
+            lang = 'en';
 
-    var js_file = document.createElement('script');
-    js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyDsb0LE0TfHPdY0yKWvtD7XkNQkpZKW_MM&language=';
-    document.getElementsByTagName('head')[0].appendChild(js_file);
-  }
+        var js_file = document.createElement('script');
+        js_file.type = 'text/javascript';
+        js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyDsb0LE0TfHPdY0yKWvtD7XkNQkpZKW_MM&language=';
+        document.getElementsByTagName('head')[0].appendChild(js_file);
+    }
 });
 
 
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.5231, lng: -122.6765},
-    zoom: 8
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 45.5231,
+            lng: -122.6765
+        },
+        zoom: 8
+    });
+    var infoWindow = new google.maps.InfoWindow({
+        map: map
+    });
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');   
 }
